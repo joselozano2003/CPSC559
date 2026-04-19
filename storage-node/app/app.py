8f259194fba72168e90805b7fee70666143c53a3
Jose Lozano <josecamilo.lozanocet@ucalgary.ca>
2026-04-19 15:36:27 -0600

    CORS fix

--- storage-node/app/app.py
+++ storage-node/app/app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 from .extensions import db
 from .routes import bp, send_heartbeat
 
@@ -8,6 +9,7 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+    CORS(app)
     db.init_app(app)
 
     with app.app_context():
